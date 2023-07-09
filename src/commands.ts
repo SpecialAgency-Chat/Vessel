d7490ddf0d21a0938310a76824df7c832dadfed8
すずねーう <suzuneu@suzuneu.com>
2023-07-09 12:28:47 +0000

    Add commands and register them with Discord API

---
+++ src/commands.ts
@@ -0,0 +1,10 @@
+import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
+
+const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
+  {
+    name: "ping",
+    description: "Respond with Pong"
+  }
+]
+
+export default commands;
