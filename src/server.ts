7a047e3dfc76cc32463c576de3ac434c36a4c724
すずねーう <suzuneu@suzuneu.com>
2023-07-09 12:28:32 +0000

    Handle ping interaction with ephemeral message

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { verifyDiscordRequest } from "@/verifyDiscordRequest";
-import { InteractionResponseType, InteractionType } from "discord-api-types/v10";
+import { InteractionResponseType, InteractionType, MessageFlags } from "discord-api-types/v10";
 import { configLogger, getLogger } from "./logger";
+import { isChatInputApplicationCommandInteraction } from "discord-api-types/utils/v10";
 
 const app = new Hono();
 
@@ -19,13 +20,30 @@ app.post("/interactions", async (c) => {
   logger.trace(c);
   const { isValid, interaction } = await verifyDiscordRequest(c);
   if (!isValid) {
-    logger.debug("Invalid request")
+    logger.debug("Invalid request");
+    c.status(400);
     return c.json({ error: "Invalid request" });
   }
   if (interaction.type === InteractionType.Ping) {
     // Handle ping
     logger.debug("Handling ping");
     return c.json({ type: InteractionResponseType.Pong });
+  }
+  if (interaction.type === InteractionType.ApplicationCommand) {
+    if (isChatInputApplicationCommandInteraction(interaction)) {
+      switch (interaction.data.name.toLowerCase()) {
+        case "ping": {
+          logger.debug("handling ping interaction");
+          return c.json({
+            type: InteractionResponseType.ChannelMessageWithSource,
+            data: {
+              content: "Pong!",
+              flags: MessageFlags.Ephemeral
+            }
+          });
+        }
+      }
+    }
   }
 
   // Unknown interaction type
