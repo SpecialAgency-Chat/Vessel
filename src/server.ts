3eaa8a63aee8aeefa77d6a6f6d2a2cad59e16336
waki285 <suzuneu@suzuneu.com>
2023-07-09 17:11:34 +0900

    Handle ping interaction

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { verifyDiscordRequest } from "@/verifyDiscordRequest";
+import { InteractionResponseType, InteractionType } from "discord-api-types/v10";
 
 const app = new Hono();
 
@@ -11,6 +12,10 @@ app.post("/interactions", async (c) => {
   const { isValid, interaction } = await verifyDiscordRequest(c);
   if (!isValid) {
     return c.json({ error: "Invalid request" });
+  }
+  if (interaction.type === InteractionType.Ping) {
+    // Handle ping
+    return c.json({ type: InteractionResponseType.Pong });
   }
 });
 
