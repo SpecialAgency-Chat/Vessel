4407731642cdbf1308b97d2ea4802e30de6a52d9
すずねーう <suzuneu@suzuneu.com>
2023-07-10 01:59:57 +0000

    Register all commands using fetch

--- src/register.ts
+++ src/register.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
-import Commands from "@/commands";
+import Commands from "./commands";
 
-dotenv.config({ path: ".dev.vars" })
+dotenv.config({ path: ".dev.vars" });
 
 const token = process.env.DISCORD_TOKEN;
 const applicationId = process.env.DISCORD_APPLICATION_ID;
@@ -11,36 +11,39 @@ if (!token) {
 }
 if (!applicationId) {
   throw new Error(
-    "The DISCORD_APPLICATION_ID environment variable is required."
+    "The DISCORD_APPLICATION_ID environment variable is required.",
   );
 }
 
-
 const url = `https://discord.com/api/v10/applications/${applicationId}/commands`;
 
-const response = await fetch(url, {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bot ${token}`
-  },
-  method: "PUT",
-  body: JSON.stringify(Commands)
-});
+async () => {
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bot ${token}`,
+    },
+    method: "PUT",
+    body: JSON.stringify(Commands),
+  });
 
-if (response.ok) {
-  console.log("Registered all commands");
-  const data = await response.json();
-  console.log(JSON.stringify(data, null, 2));
-} else {
-  console.error("Error registering commands");
-  let errorText = `Error registering commands \n ${response.url}: ${response.status} ${response.statusText}`;
-  try {
-    const error = await response.text();
-    if (error) {
-      errorText = `${errorText} \n\n ${error}`;
+  if (response.ok) {
+    console.log("Registered all commands");
+    const data = await response.json();
+    console.log(JSON.stringify(data, null, 2));
+    process.exit(0);
+  } else {
+    console.error("Error registering commands");
+    let errorText = `Error registering commands \n ${response.url}: ${response.status} ${response.statusText}`;
+    try {
+      const error = await response.text();
+      if (error) {
+        errorText = `${errorText} \n\n ${error}`;
+      }
+    } catch (err) {
+      console.error("Error reading body from request:", err);
     }
-  } catch (err) {
-    console.error("Error reading body from request:", err);
+    console.error(errorText);
+    process.exit(1);
   }
-  console.error(errorText);
-}
+};
