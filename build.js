1127773297194729e9f8c2a4a22db3f7917f6e26
すずねーう <suzuneu@suzuneu.com>
2023-07-27 00:38:07 +0000

    fix build system

--- build.js
+++ build.js
@@ -3,10 +3,11 @@ import { context } from "esbuild";
 (async () => {
   const ctx = await context({
     entryPoints: ["src/server.ts"],
-    bundle: true,
     outdir: "dist",
+    bundle: true,
+    format: "esm",
     tsconfig: "tsconfig.json",
-    minify: false,
+    minify: true,
     sourcemap: "inline",
     legalComments: "none",
     platform: "browser",
@@ -19,12 +20,14 @@ import { context } from "esbuild";
     bundle: true,
     outdir: "dist",
     tsconfig: "tsconfig.json",
-    minify: false,
+    minify: true,
     sourcemap: "inline",
     legalComments: "none",
     platform: "node",
     target: "esnext",
+    format: "esm",
     logLevel: "info",
+    banner: { js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);`}
   });
   await ctx2.rebuild();
   process.exit(0);
